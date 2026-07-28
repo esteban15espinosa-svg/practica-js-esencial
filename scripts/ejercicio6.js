0d9b99885822c8f83a30b7aa7b8bd8a44a6b6b7f
Otoniel Esteban Espinoza Contreras <esteban15espinosa@gmail.com>
2026-07-28 02:19:43 -0600

    ejercicio 6:Comparacion de valores

---
+++ scripts/ejercicio6.js
@@ -0,0 +1,10 @@
+
+const igualdadDebil = (10 == "10"); 
+const igualdadEstricta = (10 === "10");
+
+
+console.log("Igualdad débil (10 == '10'):", igualdadDebil);       
+console.log("Igualdad estricta (10 === '10'):", igualdadEstricta); 
+
+document.write("<p><strong>Igualdad débil (10 == '10'):</strong> " + igualdadDebil + "</p>");
+document.write("<p><strong>Igualdad estricta (10 === '10'):</strong> " + igualdadEstricta + "</p>");
