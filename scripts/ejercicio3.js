8c07fe00a1ec230dfddead2f9b80ce36a77fdebb
Otoniel Esteban Espinoza Contreras <esteban15espinosa@gmail.com>
2026-07-27 18:27:45 -0600

    tercer ejercicio:Operador de Asignación Compuesta

---
+++ scripts/ejercicio3.js
@@ -0,0 +1,5 @@
+let puntaje = 100;
+puntaje += 50;
+console.log("El puntaje final es: " + puntaje);
+
+document.write("<p class='resultado-write'>El nuevo puntaje es: <strong>" + puntaje + "</strong></p>");
