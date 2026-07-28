3026658f9dcef29516ebbcd6a03e08be5acdb16e
Otoniel Esteban Espinoza Contreras <esteban15espinosa@gmail.com>
2026-07-28 02:59:35 -0600

    jercicio 7 : Descripcion logica con operador and

---
+++ scripts/ejercicio7.js
@@ -0,0 +1,9 @@
+
+let nota1 = 8;
+let nota2 = 6;
+
+let ambasAprobadas = (nota1 >= 7) && (nota2 >= 7);
+
+document.write("<p><strong>¿Ambas notas son mayores o iguales a 7?:</strong> " + ambasAprobadas + "</p>");
+
+console.log("¿Ambas notas son mayores o iguales a 7?:", ambasAprobadas); 
