83fad9c1d6a1d5284a75564ca2f6117ef44d1d14
Otoniel Esteban Espinoza Contreras <esteban15espinosa@gmail.com>
2026-07-28 03:28:00 -0600

    ejercicio 8 : condicion logica con operador or

---
+++ scripts/ejercicio8.js
@@ -0,0 +1,9 @@
+
+let nota1 = 8;
+let nota2 = 6;
+
+let alMenosUnaAprobada = (nota1 >= 7) || (nota2 >= 7);
+
+document.write("<p><strong>¿Al menos una nota es mayor o igual a 7?:</strong> " + alMenosUnaAprobada + "</p>");
+
+console.log("¿Al menos una nota es mayor o igual a 7?:", alMenosUnaAprobada); 
